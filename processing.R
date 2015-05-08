728f98738413c074bce43e424d185887493cd906
Polyphant1 <chrisl1991@hotmail.co.uk>
2015-05-08 20:24:40 +0100

    completed initial analysis

--- processing.R
+++ processing.R
@@ -4,22 +4,13 @@ setwd(wd)
 
 library(tm)
 library(Narrative)
+library(RWeka)
 library(rjson)
 library(data.table)
 library(dplyr)
 
 library(zoo)
 library(ggplot2)
-
-# RMongo attempt
-# library(RMongo)
-# mg1 <- RMongo::mongoDbConnect('mydb')
-# print(RMongo::dbShowCollections(mg1))
-# 
-# query <- RMongo::dbGetQuery(rmongo.object = mg1,collection =  'tweets', "{created_at: {$gt: { $date: '2015-04-20T00:00:00Z'}}},{text: 1, favorite_count: 1,}")
-# 
-# # unpack user json
-# query.user<-lapply(query[,"user"],function(x) rjson::fromJSON(x))
 
 # read candidates data
 candidates<-read.csv(file = "candidates.csv",sep = ",")
@@ -51,7 +42,7 @@ tweets.dt[,user.statuses_count:=as.numeric(user.statuses_count)]
 tweets.dt[,user.friends_count:=as.numeric(user.friends_count)]
 tweets.dt[,user.favourites_count:=as.numeric(user.favourites_count)]
 
-rm(mongo,coll,cursor,buf)
+rm(tweets.list,mongo,coll,cursor,buf,query)
 
 # match screen name with candidates data. return ordered data from candidates
 candidate_fields<-c("party","constituency","name","gender","birth_date","honorific_prefix","party_id")
@@ -63,17 +54,10 @@ tweet.data.df<-dplyr::rbind_all(lapply(tweet.data, data.frame, stringsAsFactors 
 # convert data types
 tweet.data.df[,"birth_date"]<-lapply(tweet.data.df[,"birth_date"],as.Date)
 #tweet.data.df[,"gender"]<-lapply(tweet.data.df[,"gender"],function(x) as.String(tolower(x)))
-rm(tweet.data)
+rm(tweet.data,candidates,candidate_fields)
 
-tweet.meta.data<-data.table()
-tweet.data.df
-
-#save.image(file=paste(wd,"matched_materials.RData",sep=""))
 
 corp<-tm::VCorpus(tm::VectorSource(tweets.dt[["text"]]))
-
-# get all meta fields, ignoring content
-meta_fields<-names(tweets.dt)[!(names(tweets.dt) %in% c("text"))]
 
 clean_non_ascii <- function(x){iconv(x, "latin1", "ASCII", sub="")}
 corp.clean<-tm::tm_map(corp,content_transformer(clean_non_ascii))
@@ -85,50 +69,9 @@ corp.clean<-tm::tm_map(corp.clean,content_transformer(removeWords),stopwords("en
 
 tdm<-tm::TermDocumentMatrix(corp.clean)
 
-tm::findFreqTerms(tdm,lowfreq = 30)
+#wc<-Narrative::wordCount(t(tdm))
 
-terms<-c("nhs")
-normalise<-T
-search.result<-as.matrix(t(tdm[terms,]))
+tdm.2<-Narrative::tdmGenerator(seq(1,2,by=1),corp.clean)
 
-logic.labour<-as.logical(tweet.data.df$party=="Labour Party")
-logic.labour[is.na(logic.labour)]<-F
-xts.search<-Narrative::xtsGenerate(tweets.dt[logic.labour]$created_at,search.result[logic.labour])
-xts.search.aggregate<-Narrative::xtsAggregate(xts.search,time_aggregate="daily",normalisation = normalise)
-names(xts.search.aggregate)<-"labour"
-
-logic.ukip<-as.logical(tweet.data.df$party==c("UK Independence Party (UK I P)","UK Independence Party (UKIP)"))
-logic.ukip[is.na(logic.ukip)]<-F
-xts.search<-Narrative::xtsGenerate(tweets.dt[logic.ukip]$created_at,search.result[logic.ukip])
-xts.search.aggregate.ukip<-Narrative::xtsAggregate(xts.search,time_aggregate="daily",normalisation = normalise)
-names(xts.search.aggregate.ukip)<-"UKIP"
-
-logic.conservative<-as.logical(tweet.data.df$party=="Conservative Party")
-logic.conservative[is.na(logic.conservative)]<-F
-xts.search<-Narrative::xtsGenerate(tweets.dt[logic.conservative]$created_at,search.result[logic.conservative])
-xts.search.aggregate.conservative<-Narrative::xtsAggregate(xts.search,time_aggregate="daily",normalisation = normalise)
-names(xts.search.aggregate.conservative)<-"conservative"
-
-logic.liberals<-as.logical(tweet.data.df$party=="Liberal Democrats")
-logic.liberals[is.na(logic.liberals)]<-F
-xts.search<-Narrative::xtsGenerate(tweets.dt[logic.liberals]$created_at,search.result[logic.liberals])
-xts.search.aggregate.liberals<-Narrative::xtsAggregate(xts.search,time_aggregate="daily",normalisation = normalise)
-names(xts.search.aggregate.liberals)<-"liberals"
-
-logic.green<-as.logical(tweet.data.df$party=="Green Party")
-logic.green[is.na(logic.green)]<-F
-xts.search<-Narrative::xtsGenerate(tweets.dt[logic.green]$created_at,search.result[logic.green])
-xts.search.aggregate.green<-Narrative::xtsAggregate(xts.search,time_aggregate="daily",normalisation = normalise)
-names(xts.search.aggregate.green)<-"green"
-
-#sent.zoo<-zoo::as.zoo(xts.search.aggregate.ukip)
-sent.zoo<-zoo::as.zoo(cbind(xts.search.aggregate,
-                            xts.search.aggregate.ukip,
-                            xts.search.aggregate.conservative,
-                            xts.search.aggregate.liberals,
-                            xts.search.aggregate.green))
-
-p<-ggplot2::autoplot(na.approx(sent.zoo),facet=NULL)
-p+xlab("Year")+ylab("Absolute Count")
-
-rm(xts.search.aggregate,xts.search.aggregate,search.result,xts.search)
+#save.image(file=paste(wd,"matched_materials_small.RData",sep=""))
+load(file = "matched_materials.RData")
